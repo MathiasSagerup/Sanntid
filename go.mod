d9a74ec62c6bf9e0bf5bc3d852d6f9bd59ea5413
MathiasSagerup <mathias.sagerup@gmail.com>
2026-01-15 13:34:04 +0100

    inital commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sanntid
+
+go 1.25.5
---
+++ test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("Go is properly installed!")
+	fmt.Printf("Go version: %s\n", runtime.Version())
+	fmt.Printf("OS: %s\n", runtime.GOOS)
+	fmt.Printf("Architecture: %s\n", runtime.GOARCH)
+}
