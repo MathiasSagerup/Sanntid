941c35562dd0377eb742e5fc3de2166148eab3e5
Karoline Garsjø <karolsga@stud.ntnu.no>
2026-01-20 14:21:08 +0100

    "gjorde ferdig assignment2"

---
+++ assignment2/part2/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net"
+	"log"
+	"fmt"
+	"time"
+)
+
+func SendReceiceMessage(){
+
+	
+
+}
+
+func main(){
+	serverSendConnection, err := net.Dial("tcp", "10.100.23.11:34933")
+	if err!=nil{
+		log.Fatalln(err)
+	}
+	buffer := make([]byte, 1024)
+	length, err := serverSendConnection.Read(buffer)
+
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println("Message:", string(buffer[:length]))
+	
+	//writing to server 
+
+	msg := make([]byte, 1024) //lager minne som er 2014 byte
+	copy(msg, []byte("Hei erver")) //kopierer melidngen min inn i minneomrdået med fast størrelse 
+	serverSendConnection.Write(msg) //svarer server 
+	fmt.Println("Message sent to server")
+	
+	for {
+		n, err := serverSendConnection.Read(buffer)
+		if err!=nil{
+			log.Fatal(err)
+		}
+		fmt.Println("mottatt:", string(buffer[:n]))
+		time.Sleep(3*time.Second)
+	}
+
+
+
+
+}
