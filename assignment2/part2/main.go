83d7143a84b5450c85c7ae0eb409c5d45a6d899f
Karoline Garsjø <karolsga@stud.ntnu.no>
2026-01-24 15:45:37 +0100

    endre klasediagram og tcp

--- assignment2/part2/main.go
+++ assignment2/part2/main.go
@@ -27,21 +27,24 @@ func main(){
 
 	fmt.Println("Message:", string(buffer[:length]))
 	
-	//writing to server 
+	//writing to server and reading its answer
 
 	msg := make([]byte, 1024) //lager minne som er 2014 byte
 	copy(msg, []byte("Hei erver")) //kopierer melidngen min inn i minneomrdået med fast størrelse 
 	serverSendConnection.Write(msg) //svarer server 
 	fmt.Println("Message sent to server")
-	
+	serverSendConnection.SetDeadline(time.Now().Add(5*time.Second))
 	for {
 		n, err := serverSendConnection.Read(buffer)
 		if err!=nil{
 			log.Fatal(err)
 		}
 		fmt.Println("mottatt:", string(buffer[:n]))
-		time.Sleep(3*time.Second)
 	}
+
+	//accepting connection 
+
+	
 
 
 
