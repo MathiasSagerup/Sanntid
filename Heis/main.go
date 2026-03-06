ec4f12a3e66f07d56607bb00209d7080c9b1ccb8
MathiasSagerup <mathias.sagerup@gmail.com>
2026-03-06 14:30:24 +0100

    notes and pseudo code

--- Heis/main.go
+++ Heis/main.go
@@ -3,4 +3,18 @@ packages main
 import "fmt"
 import "Network"
 import "Driver"
+import "heis/hallcallassigner"
 
+main {
+	CommunicationModule communiacetion := InitializeHallCallCommunication()
+	HallCallAssigner hallcallassigner := InitializeHallCallAssigner(*communication)
+
+	
+
+	switch expression {
+		case newhallcalls <- hallcallassigner.InterfaceChannels.AssignedHallcalls:
+		
+	}
+
+
+}
