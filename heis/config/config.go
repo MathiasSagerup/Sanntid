4b2d28ce16108fb04e7a0d08798e69c63c7769fb
linux@stud.ntnu.no <fredrrso@stud.ntnu.no>
2026-03-19 09:30:04 +0100

    endret config til nElev = 3

--- heis/config/config.go
+++ heis/config/config.go
@@ -4,7 +4,7 @@ import "time"
 
 const (
 	N_FLOORS              = 4
-	N_ELEVATORS           = 2
+	N_ELEVATORS           = 3
 	N_BUTTONS             = 3      // HallUp, HallDown, Cab
 	PEER_TIMEOUT_DURATION = 1000   // milliseconds
 	DoorOpenDuration      = 3 * time.Second
